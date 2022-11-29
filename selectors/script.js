ca26a6d5edb229a61bae3724a5b71ea14c86ef97
Pierre M <109650634+Gollumeo@users.noreply.github.com>
2022-11-29 11:28:01 +0100

    ex2

--- selectors/script.js
+++ selectors/script.js
@@ -3,3 +3,27 @@ const importantElements = document.querySelectorAll('.important');
 importantElements.forEach(element => {
   element.setAttribute('title', 'This is an important item');
 });
+
+const images = document.querySelectorAll("img");
+
+images.forEach(img => {
+    if (!img.classList.contains("important")) {
+        img.style.display = "none";
+    }
+});
+
+const paragraphs = document.querySelectorAll("p");
+
+paragraphs.forEach(p => {
+    console.log(p.textContent);
+    if (p.className) {
+        console.log("Class name: " + p.className);
+    }
+});
+
+paragraphs.forEach(paragraph => {
+    if (!paragraph.classList.contains("special")) {
+      let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+      paragraph.style.color = randomColor;
+    }
+  });
