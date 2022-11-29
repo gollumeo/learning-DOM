fdff843463733c6c61bf7b151ba2f5ba55836c5d
Pierre M <109650634+Gollumeo@users.noreply.github.com>
2022-11-29 16:56:54 +0100

    PLEASE SEND HELP

--- create/script.js
+++ create/script.js
@@ -1,7 +1,57 @@
 let learners = ["Arno Volts", "Aurélien Mariaule", "Aurore Lemaire", "Benjamin Porsont", "Céline Verreydt", "Corentin Miserque", "Dominique Coppée", "Edouard de Romrée de Vichenet", "Hugo Goorickx", "Jofrey Houyoux", "Jonathan Manes", "Jonathan Bajoux", "Laura Wilhelmi", "Lysie Soyez", "Marnie Benalia", "Mathilde Cornelis", "Milo Bonnet", "Nadim El Nakadi", "Nathalie Vanden Abeele", "Nathalie Goffette", "Nour Everaert", "Pierre Mauriello", "Quentin Bource ", "Virginie Dourson"];
+let textColor = null;
+const color = null;
+
+function randColor() {
+    const color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+}
+
+function lightOrDark(color) {
+
+    // Variables for red, green, blue values
+    var r, g, b, hsp;
+    
+    // Check the format of the color, HEX or RGB?
+    if (color.match(/^rgb/)) {
+
+        // If RGB --> store the red, green, blue values in separate variables
+        color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
+        
+        r = color[1];
+        g = color[2];
+        b = color[3];
+    } 
+    else {
+        
+        // If hex --> Convert it to RGB: http://gist.github.com/983661
+        color = +("0x" + color.slice(1).replace( 
+        color.length < 5 && /./g, '$&$&'));
+
+        r = color >> 16;
+        g = color >> 8 & 255;
+        b = color & 255;
+    }
+    
+    // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+    hsp = Math.sqrt(
+    0.299 * (r * r) +
+    0.587 * (g * g) +
+    0.114 * (b * b)
+    );
+
+    // Using the HSP value, determine whether the color is light or dark
+    if (hsp>127.5) {
+
+        textColor = 'black';
+    } 
+    else {
+
+        textColor = 'white';
+    }
+}
 
 // randomizing in which order the learners are sorted so we can fake the random creation of the paragraphs
-learners.sort(() => Math.random() - 0.5);
+learners.sort(() => 0.5 - Math.random());
 
 const article = document.querySelector('article');
 
@@ -10,20 +60,10 @@ for (let i = 0; i < learners.length; i++) {
     const newSection = document.createElement('section');
 
     //random background-color
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
+    randColor();
+    newSection.style.backgroundColor = color;
 
-    color = `rgb(${r}, ${g}, ${b})`;
-
-    //check background brightness to change text color
-    let brightness = (parseInt(color.substring(1, 3), 16) * 0.299) + (parseInt(color.substring(3, 5), 16) * 0.587) + (parseInt(color.substring(5, 7), 16) * 0.114);
-    let textColor;
-    if ((brightness > 150)) {
-        textColor = 'black';
-    } else {
-        textColor = 'white';
-    }
+    lightOrDark(color);
 
     //create paragraph
     const newParagraph = document.createElement('p');
@@ -31,6 +71,15 @@ for (let i = 0; i < learners.length; i++) {
     newParagraph.appendChild(document.createTextNode(learners[i]));
     newSection.appendChild(newParagraph);
 
+    
+
+    // if (newSection.style.backgroundColor) {
+    //     break;
+    // } else {
+    //     newSection.style.backgroundColor = color;
+    // }
+
     //append section to article
     article.appendChild(newSection);
+
 }
--- introduction/script.js
+++ introduction/script.js
@@ -1,6 +1,9 @@
 console.log(document.title);
+
 document.title = "Modifying the DOM";
+
 document.body.style.backgroundColor = "#FF69B4";
+
 document.body.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 
 let bodyChildren = document.body.children;
